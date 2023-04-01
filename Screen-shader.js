287fbfc04247508b92d571ec263ac52897d408ec
Ayyub Muhammad <122996864+Id-Yuu@users.noreply.github.com>
2023-04-01 17:06:06 +0700

    screen-shader + css

---
+++ Screen-shader.js
@@ -0,0 +1,11 @@
+/*
+	<div id='readmode'></div>
+	<button class='readmodec'>Read Mode!!</button>
+	- https://github.com/Id-Yuu/Css
+*/
+
+$(document).ready(function(){
+  $(".readmodec").click(function(){
+    $("#readmode").toggleClass("active");
+  });
+}); 
