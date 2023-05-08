7269b21cc26714b596facea70ae02c7ddd4f8d3f
Yuu <122996864+Id-Yuu@users.noreply.github.com>
2023-05-09 03:56:38 +0700

    pwa

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = `arlethdesign-v1`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/',
+      'https://rawcdn.githack.com/Id-Yuu/Css/15a27deda31c951821e567487a4489c90507a61d/styles.css',
+      'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
